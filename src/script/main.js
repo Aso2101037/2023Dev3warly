4e9e12b764029fd216a7c4068d18ae9c94b23954
anusFuck <2101026@s.asojuku.ac.jp>
2023-05-26 00:28:58 +0900

    コンポーネント作成
    使いまわすヘッダーのコンポーネントのデザインを作成しました

--- src/script/main.js
+++ src/script/main.js
@@ -1,8 +1,16 @@
 Vue.component('header-componet',{
     template:
-    `<div>
-        
-    </div>`
+    `<div class="header">
+       <div class="title-logo">
+            <span class="title">トラスタ</span><img src="./images/plean-log.svg" class="logo" alt="飛行機のロゴ">
+        </div>
+        <div class="header-button">
+            <img src="./images/profile.svg" alt="">
+            <img src="./images/search.svg" alt="">
+            <img src="./images/square-plus.svg" alt="">
+            <div class="login-div"><img src="./images/login.svg" alt=""><span>Login</span></div>
+        </div>
+     </div>`,
     })
 const app = new Vue({
     el:'#app',
