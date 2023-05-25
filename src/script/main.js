9d2c40f45cf4682f114f4f2b35902d99dc1051ba
anusFuck <2101026@s.asojuku.ac.jp>
2023-05-25 12:14:58 +0900

    Update main.js

--- src/script/main.js
+++ src/script/main.js
@@ -1,4 +1,11 @@
 Vue.component('component-hedder',{
     templete:
-    ''
+    `<div class="header">
+        <div class="header-logo"></div>
+        <div class="header-button"></div>
+    </div>`
+});
+
+const app = new Vue({
+    el:'#app',
 })
