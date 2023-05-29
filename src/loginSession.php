d7ba96374c5aee208a8f0ceae7835fa1089290a3
2201111-kishikawa <114889553+2201111-kishikawa@users.noreply.github.com>
2023-05-29 20:47:13 +0900

    メッセージ追加
    メッセージ加えました

--- src/loginSession.php
+++ src/loginSession.php
@@ -2,8 +2,7 @@
 class LoginSession{
     public function session_email_pass($pass,$email){
         session_start();
-
-        // セッションに変数を保存
+        // ハッシュ化したパスワードを引数として与えてください
         $_SESSION['email'] = $email;
         $_SESSION['pass'] = $pass;
     }
