91167045b028686729ec86c024e0aa8cb2574e3f
2201111-kishikawa <114889553+2201111-kishikawa@users.noreply.github.com>
2023-05-29 20:45:19 +0900

    sessionにemail,passを追加するプログラム
    追加しました

--- src/loginSession.php
+++ src/loginSession.php
@@ -1,7 +1,11 @@
 <?php
 class LoginSession{
-    public function session_id_pass($pass,$email){
-        
+    public function session_email_pass($pass,$email){
+        session_start();
+
+        // セッションに変数を保存
+        $_SESSION['email'] = $email;
+        $_SESSION['pass'] = $pass;
     }
 }
 ?>
