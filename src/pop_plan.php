0ce98dc46d0b3936b94da5cf397b60b521fafdc0
yuhei12345 <2101024@s.asojuku.ac.jp>
2023-06-08 11:52:56 +0900

    人気画面　完了

---
+++ src/pop_plan.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/populer.css">
+</head>
+<body>
+<div id="app">
+        <header-component @clicksearch="ModalSeach"></header-component>
+        <div :class="{'is-active': isActive }" class="modal-div">
+            <div class="modal-body">
+                <div class="modal-plan" onclick="location.href='./search-plan.php'">
+                     <p>旅行プラン検索</p>
+                     <img src="./images/post-plan.svg" style="width: 5rem; height: 8rem;">
+                </div>
+                <div class="modal-kankou">
+                     <p>観光名所検索</p>
+                     <img src="./images/post-kankou.svg" style="width: 6rem; height: 7rem;" alt="">
+                </div>
+                 <div class="modal-food">
+                     <p>飲食店検索</p>
+                    <img src="./images/post-food.svg" style="width: 6rem; height: 7rem;" alt="">
+                </div>
+            </div>
+        </div>
+</div>
+<nav>
+  <ol class="breadcrumb">
+    <li class="list"><a href="home.php">Home</a></li>>
+    <li class="list">人気の旅行プラン</li>
+  </ol>
+</nav>
+<div id="card">
+    <card-component class="card-co"></card-component>
+    <card-component class="card-co"></card-component>
+    <card-component class="card-co"></card-component>
+</div>
+        
+     
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script src="./script/header.js"></script>
+<script src="./script/card.js"></script>
+</body>
+</html>
