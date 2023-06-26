2ea1d4b372de7d495ce2f47f82057e5a1662065d
anusFuck <2101026@s.asojuku.ac.jp>
2023-06-27 07:08:14 +0900

    スタンプラリー地方作成途中
    スタンプラリー地方作成途中あとは中部から九州までのdivを作成したら完成

--- src/stamp-region.php
+++ src/stamp-region.php
@@ -48,59 +48,21 @@
             </div>
         </div>
     </div>
-    <!-- プロフィール部分 -->
-    <div class="profille-container">
-        <div class="center-profille" >
-            <div class="profille-left"  onclick="location.href='./stamp-region.php'">
-                <img src="./images/icon.svg" class="top-img" alt="トプ画">
-                <div class="like-list"><span>いいね一覧</span></div>
+    <div class="stamp-container">
+        <div class="center-stamp">
+            <div class="triplog-post">
+                <h2 class="trip-font">Trip Log</h2>
+                <h2 class="total-font">総投稿数数<span>~</span>件</h2>
             </div>
-            <div class="profille-right">
-                <div class="titles-name">
-                    <div class="nickname"><span>ニックネーム</span></div>
-                    <div class="title-holder"><span>称号名</span></div>
+            <div class="region-post">
+                <div class="kanto-div">
+                    <div class="hokkaido"><span>北海道</span></div>
+                    <div class="kanto"><span>関東</span></div>
                 </div>
-                <div class="comment-div"><span>コメント</span></div>
-            </div>
+                <div class="kinki-div"></div>
+                <div class="kyushu-div"></div>
+            </div>    
         </div>
-    </div>
-    <!-- 旅行日記部分 -->
-    <div class="trip-container">
-        <div class="center-trip">
-            <div class="trip-left">
-                <img src="./images/trip-memory.svg" alt=""><span>旅行日記</span>
-            </div>
-            <div class="trip-right">
-                <div class="total-post"><span>総投稿数<span>～</span>件</span></div>
-                <div class="place-visite"><span>訪れた場所<span>～</span>カ所</span></div>
-            </div>
-        </div>
-    </div>
-    <!-- 最近の投稿を表示する部分 -->
-    <div class="new-container">
-        <div class="center-post">
-            <div class="new-title-delete">
-                <div class="new-post"><span>最新の投稿</span></div>
-                <div class="post-delete"><span>投稿削除</span></div>
-            </div>
-            <div class="new-posts">
-                <div class="post-card">
-                    <div class="post-type">旅行プラン</div>
-                    <div class="new-img"></div>
-                </div>
-                <div class="post-card">
-                    <div class="post-type">観光名所</div>
-                    <div class="new-img"></div>
-                </div>
-                <div class="post-card">
-                    <div class="post-type">飲食店</div>
-                    <div class="new-img"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="back-button">
-        <button class="back"onclick="history.back(-1);" >＜戻る</button>
     </div>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
