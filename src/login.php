c36b9d9dd6de9da2b2622400780bc386a47bb2d0
anusFuck <2101026@s.asojuku.ac.jp>
2023-06-29 10:40:38 +0900

    ログイン画面修正完了

--- src/login.php
+++ src/login.php
@@ -62,13 +62,7 @@
             <input type="password" name = "inputPassword" id="inputPassword" class="form-control mx-auto" placeholder="パスワードを入力してください" required>
         </div>
         <button class="back" type="button" onclick="history.back(-1);">＜戻る</button>
-<<<<<<< Updated upstream
-        <button class="log-in" type="submit">Login</button>
-=======
         <button class="login" type="submit">Login</button>
->>>>>>> Stashed changes
-
-        
     </form>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
