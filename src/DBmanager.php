8776f2f851ecba8837d0ab9e613dd1575a840ea2
2201111-kishikawa <114889553+2201111-kishikawa@users.noreply.github.com>
2023-07-06 11:57:51 +0900

    投稿関係のメソッド追加
    dbmanager.phpに投稿関係のメソッド追加

--- src/DBmanager.php
+++ src/DBmanager.php
@@ -271,6 +271,126 @@ public function deletePlanPostDtlTbl($getid){
         echo "<script>alert('削除に失敗しました。');</script>";
     }
 }
+    //旅行プラン投稿機能
+public function plan_post($plan_spot_name,$plan_spot_start_time,$plan_spot_finish_time,$plan_spot_address,$plan_spot_public_transport,$plan_spot_travel_time,$plan_spot_comment,$plan_spot_image,$plan_title,$release,$plan_day,$prefecture_id){
+        $pdo = $this->dbConnect();
+        session_start();
+        $user_id = $_SESSION['user_id'];
+        $sql = "INSERT INTO plan_post(plan_post_id,user_id,plan_title,release,plan_day)VALUES(?,?,?,?,?)";
+        $ps = $pdo->prepare($sql);
+        $min = 1;
+        $max = 999999;
+        $plan_post_id = mt_rand($min, $max);//ランダムにplan_post_idを作成
+        $ps->bindValue(1,$plan_post_id,PDO::PARAM_STR);
+        $ps->bindValue(2,$user_id,PDO::PARAM_STR);
+        $ps->bindValue(3,$plan_title,PDO::PARAM_STR);
+        $ps->bindValue(4,$release,PDO::PARAM_STR);
+        $ps->bindValue(5,$plan_day,PDO::PARAM_STR);  
+        $ps->execute();
+        //ここまでplan_postテーブルへの追加。ここからpostテーブルへの追加
+        $sql = "INSERT INTO post(posts_id,user_id,plan_post_id,tourist_spot_id,restaurant_post_id,prefecture_id,regions_id)VALUES(?,?,?,?,?,?,?)";
+        $ps = $pdo->prepare($sql);
+        $ps->bindValue(1,0,PDO::PARAM_STR);
+        $ps->bindValue(2,$user_id,PDO::PARAM_STR);
+        $ps->bindValue(3,$plan_post_id,PDO::PARAM_STR);
+        $ps->bindValue(4,0,PDO::PARAM_STR);
+        $ps->bindValue(5,0,PDO::PARAM_STR);
+        $ps->bindValue(6,$prefecture_id,PDO::PARAM_STR);
+        $ps->bindValue(7,0,PDO::PARAM_STR);
+        $ps->execute();
+        
+        //ここからplan_post_detailテーブルへの追加
+        $sql = "INSERT INTO plan_post_detail(plan_post_id,plan_post_detail_id,user_id,plan_spot_name,plan_spot_start_time,plan_spot_finish_time,plan_spot_address,plan_spot_public_transport,plan_spot_travel_time,plan_spot_comment,plan_spot_image)VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+        $ps = $pdo->prepare($sql);
+        $ps->bindValue(1,$plan_post_id,PDO::PARAM_STR);
+        $ps->bindValue(2,0,PDO::PARAM_STR);
+        $ps->bindValue(3,$user_id,PDO::PARAM_STR);
+        $ps->bindValue(4,$plan_spot_name,PDO::PARAM_STR);
+        $ps->bindValue(5,$plan_spot_start_time,PDO::PARAM_STR);
+        $ps->bindValue(6,$plan_spot_finish_time,PDO::PARAM_STR);
+        $ps->bindValue(7,$plan_spot_address,PDO::PARAM_STR);  
+        $ps->bindValue(8,$plan_spot_public_transport,PDO::PARAM_STR);   
+        $ps->bindValue(9,$plan_spot_travel_time,PDO::PARAM_STR);
+        $ps->bindValue(10,$plan_spot_comment,PDO::PARAM_STR);  
+        $ps->bindValue(11,$plan_spot_image,PDO::PARAM_STR);    
+        $ps->execute();
+}
+//飲食店投稿機能
+public function restaurant_post($restaurant_title,$restaurant_image,$restaurant_name,$restaurant_comment,$restaurant_address,$restaurant_telephone_number,$restaurant_start_time,$restaurant_finish_time,$restaurant_budget,$restaurant_category_id,$restaurant_date,$restaurant_release){
+        $pdo = $this->dbConnect();
+        $sql = "INSERT INTO restaurant_post(restaurant_post_id,restaurant_title,restaurant_image,restaurant_name,restaurant_comment,restaurant_address,restaurant_telephone_number,restaurant_start_time,restaurant_finish_time,restaurant_budget,restaurant_category_id,restaurant_date,restaurant_release)VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        $ps = $pdo->prepare($sql);
+        $ps->bindValue(1,0,PDO::PARAM_STR);
+        $ps->bindValue(2,$restaurant_title,PDO::PARAM_STR);
+        $ps->bindValue(3,$restaurant_image,PDO::PARAM_STR);
+        $ps->bindValue(4,$restaurant_name,PDO::PARAM_STR);
+        $ps->bindValue(5,$restaurant_comment,PDO::PARAM_STR);
+        $ps->bindValue(6,$restaurant_address,PDO::PARAM_STR);  
+        $ps->bindValue(7,$restaurant_telephone_number,PDO::PARAM_STR);   
+        $ps->bindValue(8,$restaurant_start_time,PDO::PARAM_STR);
+        $ps->bindValue(9,$restaurant_finish_time,PDO::PARAM_STR);  
+        $ps->bindValue(10,$restaurant_budget,PDO::PARAM_STR);   
+        $ps->bindValue(11,$restaurant_category_id,PDO::PARAM_STR);  
+        $ps->bindValue(12,$restaurant_date,PDO::PARAM_STR); 
+        $ps->bindValue(13,$restaurant_release,PDO::PARAM_STR); 
+        $ps->execute();
+        //ここからpostテーブルへの追加
+        $sql = "SELECT * FROM restaurant_post WHERE restaurant_comment = ?";
+        $ps = $pdo->prepare($sql);
+        $ps->bindValue(1,$restaurant_comment,PDO::PARAM_STR);
+        $ps->execute();
+        $searchArray = $ps->fetchAll();
+        $restaurant_post_id = $searchArray['restaurant_post_id'];
+        session_start();
+        $user_id = $_SESSION['user_id'];
+        $sql = "INSERT INTO post(posts_id,user_id,plan_post_id,tourist_spot_id,restaurant_post_id,prefecture_id,regions_id)VALUES(?,?,?,?,?,?,?)";
+        $ps = $pdo->prepare($sql);
+        $ps->bindValue(1,0,PDO::PARAM_STR);
+        $ps->bindValue(2,$user_id,PDO::PARAM_STR);
+        $ps->bindValue(3,0,PDO::PARAM_STR);
+        $ps->bindValue(4,$restaurant_post_id,PDO::PARAM_STR);
+        $ps->bindValue(5,0,PDO::PARAM_STR);
+        $ps->bindValue(6,0,PDO::PARAM_STR);
+        $ps->bindValue(7,0,PDO::PARAM_STR);
+        $ps->execute();
+}
+//観光名所投稿機能
+public function tourist_spot($tourist_spot_name,$tourist_spot_address,$tourist_spot_image,$tourist_spot_start,$tourist_spot_end,$tourist_spot_title,$tourist_spot_comment,$category_id,$tourist_spot_day,$tourist_release){
+        $pdo = $this->dbConnect();
+        $sql = "INSERT INTO tourist_spot(tourist_spot_id,tourist_spot_name,tourist_spot_address,tourist_spot_image,tourist_spot_start,tourist_spot_end,tourist_spot_title,tourist_spot_comment,category_id,tourist_spot_day,tourist_release)VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
+        $ps = $pdo->prepare($sql);
+        $ps->bindValue(1,0,PDO::PARAM_STR);
+        $ps->bindValue(2,$tourist_spot_name,PDO::PARAM_STR);
+        $ps->bindValue(3,$tourist_spot_address,PDO::PARAM_STR);
+        $ps->bindValue(4,$tourist_spot_image,PDO::PARAM_STR);
+        $ps->bindValue(5,$tourist_spot_start,PDO::PARAM_STR);
+        $ps->bindValue(6,$tourist_spot_end,PDO::PARAM_STR);
+        $ps->bindValue(7,$tourist_spot_title,PDO::PARAM_STR);  
+        $ps->bindValue(8,$tourist_spot_comment,PDO::PARAM_STR);   
+        $ps->bindValue(9,$category_id,PDO::PARAM_STR);
+        $ps->bindValue(10,$tourist_spot_day,PDO::PARAM_STR);  
+        $ps->bindValue(11,$tourist_release,PDO::PARAM_STR);
+        $ps->execute();
+        //ここからpostテーブルへの追加
+        $sql = "SELECT * FROM tourist_spot WHERE tourist_spot_comment = ?";
+        $ps = $pdo->prepare($sql);
+        $ps->bindValue(1,$tourist_spot_comment,PDO::PARAM_STR);
+        $ps->execute();
+        $searchArray = $ps->fetchAll();
+        $tourist_spot_id = $searchArray['tourist_spot_id'];
+        session_start();
+        $user_id = $_SESSION['user_id'];
+        $sql = "INSERT INTO post(posts_id,user_id,plan_post_id,tourist_spot_id,restaurant_post_id,prefecture_id,regions_id)VALUES(?,?,?,?,?,?,?)";
+        $ps = $pdo->prepare($sql);
+        $ps->bindValue(1,0,PDO::PARAM_STR);
+        $ps->bindValue(2,$user_id,PDO::PARAM_STR);
+        $ps->bindValue(3,0,PDO::PARAM_STR);
+        $ps->bindValue(4,0,PDO::PARAM_STR);
+        $ps->bindValue(5,$tourist_spot_id,PDO::PARAM_STR);
+        $ps->bindValue(6,0,PDO::PARAM_STR);
+        $ps->bindValue(7,0,PDO::PARAM_STR);
+        $ps->execute();
+}
 }
 ?>
 
