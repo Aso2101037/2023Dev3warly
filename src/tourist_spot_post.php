7ba71ba49127da29725a901c9dfc37ee0f800637
2201111-kishikawa <114889553+2201111-kishikawa@users.noreply.github.com>
2023-07-06 16:25:22 +0900

    formの追加
    tourist_post.phpにformを追加しました

--- src/tourist_spot_post.php
+++ src/tourist_spot_post.php
@@ -30,6 +30,7 @@
             </div>
         </div>
 </div>
+<form action="tourist_post_kakunin1.php" method="post">
     <div class="flex">
         <div class="left">
             <div class="sinkisakusei">新規投稿を作成</div>
@@ -129,6 +130,7 @@
         <button class="back" type="button" onclick="history.back(-1);">＜戻る</button>
         <button class="sign-up" type="submit">Share</button>
     </div>
+</form>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
