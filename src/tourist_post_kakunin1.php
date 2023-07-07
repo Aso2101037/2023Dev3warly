33a80114dbe47861178aeb8d851491cac581eef1
2201111-kishikawa <114889553+2201111-kishikawa@users.noreply.github.com>
2023-07-07 09:12:17 +0900

    メッセージ追加

--- src/tourist_post_kakunin1.php
+++ src/tourist_post_kakunin1.php
@@ -12,7 +12,7 @@
     require_once "DBmanager.php";
     $dbm = new DBManager();
     $dbm->tourist_spot($form_tourist_name,$form_address_text,0,0,0,$form_title,$form_tourist_comment,$category,0,$form_koukai);
-    echo "DB保存完了";
-    echo $form_koukai;
+    echo "DB保存完了<br>";
+    echo $form_koukai+"<br>";
     echo $form_title;
 ?>
