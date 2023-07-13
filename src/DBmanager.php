d99d2755e64d239ebb8d70d29e10ae6139d8d03d
yuunaa10 <115201142+yuunaa10@users.noreply.github.com>
2023-07-13 14:35:27 +0900

    観光名所排除コード追加

--- src/DBmanager.php
+++ src/DBmanager.php
@@ -272,7 +272,26 @@ public function deletePlanPostDtlTbl($getid){
     }
 }
 
+//ボタンを押下してデータベース内の観光名所削除する
+public function deleteToristSpotTbl($getid){
+    $pdo = $this->dbConnect();
+    // 削除対象のIDを取得
+    $id = $_POST['id'];
 
+    // データを削除するクエリを実行
+    $sql = "DELETE FROM tourist_spot WHERE tourist_spot_id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+
+    if ($stmt->execute()) {
+        // 削除成功時の処理
+        echo "<script>alert('削除が成功しました。');</script>";
+    } else {
+        // 削除失敗時の処理
+        echo "<script>alert('削除に失敗しました。');</script>";
+    }
+}
 }
 ?>
 
