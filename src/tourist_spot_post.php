4938498887399b666605b8a1d3fc6423e6c6c9af
yuhei12345 <2101024@s.asojuku.ac.jp>
2023-07-13 11:46:42 +0900

    ヘッダー投稿追加

--- src/tourist_spot_post.php
+++ src/tourist_spot_post.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>トラスタ</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="./style/style.css">
     <link rel="stylesheet" href="./style/tourist_spot_post-style.css">
 </head>
@@ -18,8 +17,9 @@
                 exit();
             }
 ?>
+<!-- header部分↓↓ -->
 <div id="app">
-        <header-component @clicksearch="ModalSeach"></header-component>
+    <header-component @clicksearch="ModalSeach" @clickpost="ModalPost"></header-component>
         <div :class="{'is-active': isActive }" class="modal-div">
             <div class="modal-body">
                 <div class="modal-plan" onclick="location.href='./search-plan.php'">
@@ -36,7 +36,24 @@
                 </div>
             </div>
         </div>
-</div>
+             <!-- -投稿のモーダル -->
+             <div :class="{'post-active': postActive }" class="modal-div-post">
+            <div class="modal-body-post">
+                <div class="modal-plan-post" onclick="location.href='./plan-post.php'">
+                     <p>旅行プラン投稿</p>
+                     <img src="./images/post-plan.svg" style="width: 5rem; height: 8rem;">
+                </div>
+                <div class="modal-kankou-post" onclick="location.href='./tourist_spot_post.php'">
+                     <p>観光名所投稿</p>
+                     <img src="./images/post-kankou.svg" style="width: 6rem; height: 7rem;" alt="">
+                </div>
+                 <div class="modal-food-post" onclick="location.href='./restaurant_post.php'">
+                     <p>飲食店投稿</p>
+                    <img src="./images/post-food.svg" style="width: 6rem; height: 7rem;" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
 <form action="tourist_post_kakunin1.php" method="post">
     <div class="flex">
         <div class="left">
@@ -138,8 +155,6 @@
         <button class="sign-up" type="submit">Share</button>
     </div>
 </form>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <script src="./script/header.js"></script>
 </body>
