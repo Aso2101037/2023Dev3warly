531da507249e9493aa49e7a084fd984693e01164
yuunaa10 <115201142+yuunaa10@users.noreply.github.com>
2023-07-14 10:13:46 +0900

    id変更

--- src/deletePlanPostDtlTbl-1.php
+++ src/deletePlanPostDtlTbl-1.php
@@ -1,6 +1,6 @@
 <?php
     require 'DBManager.php';
     $dbmng = new DBManager();
-    $userList = $dbmng->deletePlanPostDtlTbl($_POST['id']);
+    $userList = $dbmng->deletePlanPostDtlTbl($_POST['tourist_spot_id']);
 
 ?>
