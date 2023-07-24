e7954eed810c06748e9455015206d69a96161191
anusFuck <2101026@s.asojuku.ac.jp>
2023-07-24 10:02:47 +0900

    最新順に表示されるようにした

--- src/DBmanager.php
+++ src/DBmanager.php
@@ -318,7 +318,7 @@ class DBManager
         }
     }
     public function getAllRestranImg(){
-        $pdo = $this->dbConnect()->prepare("SELECT restaurant_post_id , restaurant_image FROM restaurant_post");
+        $pdo = $this->dbConnect()->prepare("SELECT restaurant_post_id , restaurant_image FROM restaurant_post ORDER BY restaurant_post_id DESC");
         $pdo->execute();
     
         $images = array();
@@ -335,7 +335,7 @@ class DBManager
     }
 
     public function getAllTouristImg(){
-        $pdo = $this->dbConnect()->prepare("SELECT tourist_spot_id , tourist_spot_image FROM tourist_spot");
+        $pdo = $this->dbConnect()->prepare("SELECT tourist_spot_id , tourist_spot_image FROM tourist_spot ORDER BY tourist_spot_id DESC");
         $pdo->execute();
 
         $tourist_images = array();
@@ -349,14 +349,14 @@ class DBManager
     }
 
     public function getAllRestranData(){
-        $pdo = $this->dbConnect()->prepare("SELECT restaurant_post_id,restaurant_title,restaurant_name, restaurant_comment, restaurant_address, restaurant_start_time, restaurant_finish_time, restaurant_budget, restaurant_category_id, restaurant_date, restaurant_release FROM restaurant_post");
+        $pdo = $this->dbConnect()->prepare("SELECT restaurant_post_id,restaurant_title,restaurant_name, restaurant_comment, restaurant_address, restaurant_start_time, restaurant_finish_time, restaurant_budget, restaurant_category_id, restaurant_date, restaurant_release FROM restaurant_post ORDER BY restaurant_post_id DESC");
         $pdo->execute();
         $data = $pdo->fetchAll();
        return  json_encode($data);
     }
 
     public function getAllTouristData(){
-        $pdo = $this->dbConnect()->prepare("SELECT tourist_spot_id, tourist_spot_name,tourist_spot_address,tourist_spot_start,tourist_spot_end,tourist_spot_title,tourist_spot_comment,category_id,plan_spot_day,tourist_release FROM tourist_spot");
+        $pdo = $this->dbConnect()->prepare("SELECT tourist_spot_id, tourist_spot_name,tourist_spot_address,tourist_spot_start,tourist_spot_end,tourist_spot_title,tourist_spot_comment,category_id,plan_spot_day,tourist_release FROM tourist_spot ORDER BY tourist_spot_id DESC");
         $pdo->execute();
         $Touristdata = $pdo->fetchAll();
         return json_encode($Touristdata);
