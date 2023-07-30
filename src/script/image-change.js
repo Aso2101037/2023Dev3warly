2ab778bc9ff433fd2d68525df5fede1de7f388cc
ke1j1 <120440338+Aso2101037@users.noreply.github.com>
2023-07-30 12:33:08 +0900

    a

--- src/script/image-change.js
+++ src/script/image-change.js
@@ -1,16 +1,3 @@
-// function imageChange() {
-//     var fileInput = document.getElementById('image');
-//     var image = document.getElementById('image-add');
-//     if (fileInput.files && fileInput.files[0]) {
-//       var reader = new FileReader();
-  
-//       reader.onload = function (e) {
-//         image.src = e.target.result;
-//       };
-  
-//       reader.readAsDataURL(fileInput.files[0]);
-//     }
-//   }
 function imageChange(){
   //ファイルの情報を取得
     var fileInput = document.getElementById("image");
@@ -37,12 +24,14 @@ function imageChange(){
       reader.readAsDataURL(file);
     }
   }
+
 //ボタンを押下すると、写真ファイルの選択になる動作
 document.getElementById('image_select').addEventListener('click', function(event) {
   event.preventDefault(); // デフォルトの動作をキャンセル
 
   document.getElementById('image').click();
 });
+
   function spotChange() {
     var fileInput = document.getElementById('spot');
     var image = document.getElementById('spot_image');
@@ -58,6 +47,34 @@ document.getElementById('image_select').addEventListener('click', function(event
     }
   }
 
+//旅行プラン投稿画像追加
+  function imagePranChange(){
+    //ファイルの情報を取得
+      var fileInput = document.getElementById("up-load");
+      var file = fileInput.files[0];
+  
+      // FileReaderオブジェクトを作成
+      var reader = new FileReader();
+  
+      // 画像が読み込まれた時の処理を定義
+      reader.onload = function (e) {
+        // 画像を表示する要素を取得
+        var imageAdd = document.getElementById("image-add");
+        // 画像のパスを設定
+        imageAdd.style.backgroundImage = "url("+e.target.result+")";
+  
+      };
+        var defaultImage = document.querySelector(".logo-img");
+        defaultImage.style.display ="none";
+        var labelFont = document.querySelector(".img-select");
+        labelFont.style.opacity = "0.3";
+  
+      // 画像を読み込む
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+    }
+
   //ボタンを押下すると、写真ファイルの選択になる動作
   // ※このコード同じファイルに2つあると動かんくなるから注意
 document.getElementById('spot_image_select').addEventListener('click', function(event) {
@@ -65,3 +82,17 @@ document.getElementById('spot_image_select').addEventListener('click', function(
 
   document.getElementById('spot').click();
 });
+
+// function imageChange() {
+//     var fileInput = document.getElementById('image');
+//     var image = document.getElementById('image-add');
+//     if (fileInput.files && fileInput.files[0]) {
+//       var reader = new FileReader();
+  
+//       reader.onload = function (e) {
+//         image.src = e.target.result;
+//       };
+  
+//       reader.readAsDataURL(fileInput.files[0]);
+//     }
+//   }
